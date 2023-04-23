a26fa850e87362c248fd7d86b7d33c007de5747d
nathan-kedinger <51140084+nathan-kedinger@users.noreply.github.com>
2023-04-23 15:53:43 +0200

    Modifying roles for symfony

--- public/api_vooov/voice_style/read.php
+++ public/api_vooov/voice_style/read.php
@@ -5,7 +5,7 @@
     $table = "voice_style";
 
     // Datas
-    $arguments = $tabVoiceStyle;// Replace with the good tab
+    $arguments = $tabVoiceStyleRead;// Replace with the good tab
 
     // SQL request
     $sql = "SELECT * FROM " . $table; // It is possible to add a join after that
